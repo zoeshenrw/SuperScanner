06258f79d313bb6f95fc4e7b09078a6bf93aeca5
Zoe Shen <rs6753@nyu.edu>
2022-10-26 18:36:56 -0400

    updated login feature

--- custom.js
+++ custom.js
@@ -18,17 +18,27 @@ function clearInputError(inputElement) {
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
+    const forgotPasswordForm = document.querySelector("#forgotPassword");
 
     document.querySelector("#linkCreateAccount").addEventListener("click", e =>{
         e.preventDefault();
         loginForm.classList.add("form--hidden");
         createAccountForm.classList.remove("form--hidden");
+        forgotPasswordForm.classList.add("form--hidden");
     });
 
     document.querySelector("#linkLogin").addEventListener("click", e =>{
         e.preventDefault();
         loginForm.classList.remove("form--hidden");
         createAccountForm.classList.add("form--hidden");
+        forgotPasswordForm.classList.add("form--hidden");
+    });
+
+    document.querySelector("#linkForgotPassword").addEventListener("click", e =>{
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+        forgotPasswordForm.classList.remove("form--hidden");
     });
 
     loginForm.addEventListener("submit", e=>{
