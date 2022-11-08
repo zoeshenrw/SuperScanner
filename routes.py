bbe9c20b84e8702842cbcdffc1d781346fb59851
crystalwjy <jw5909@nyu.edu>
2022-11-07 22:50:05 -0500

    new about and scanner pages and styles

--- routes.py
+++ routes.py
@@ -2,31 +2,31 @@ from config import app, conn
 from flask import render_template, redirect, url_for, flash, request, Response
 from forms import LoginForm
 from flask import session
-import cv2
-import pyzbar.pyzbar as pyzbar
+# import cv2
+# import pyzbar.pyzbar as pyzbar
 
-camera=cv2.VideoCapture(0)
-global value 
-def read_qr_code(frame):
-    try:
-        detect = cv2.QRCodeDetector()
-        value, points, straight_qrcode = detect.detectAndDecode(frame)
-        return value
-    except:
-        return None
-def generate_frames():
-    while True:
-        ## read the camera frame
-        success,frame=camera.read()
-        if not success:
-            break
-        else:
-            value = read_qr_code(frame)
-            if value:
-                print(value)
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
-        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+# camera=cv2.VideoCapture(0)
+# global value 
+# def read_qr_code(frame):
+#     try:
+#         detect = cv2.QRCodeDetector()
+#         value, points, straight_qrcode = detect.detectAndDecode(frame)
+#         return value
+#     except:
+#         return None
+# def generate_frames():
+#     while True:
+#         ## read the camera frame
+#         success,frame=camera.read()
+#         if not success:
+#             break
+#         else:
+#             value = read_qr_code(frame)
+#             if value:
+#                 print(value)
+#             ret,buffer=cv2.imencode('.jpg',frame)
+#             frame=buffer.tobytes()
+#         yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/')
@@ -34,19 +34,28 @@ def generate_frames():
 def home_page():    
     return render_template('home/home.html')
 
-@app.route('/scanned')
-def scanned_page():  
-    try:  
-        return redirect(value)
-    except:
-        return redirect(url_for("video"))
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
 
-@app.route('/video',methods=['GET'])
-def video():
-    try: 
-        return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
-    except:
-        return redirect(url_for("scanned_page"))
+@app.route('/scanner')
+def scanner_page():
+    return render_template('scanner.html')
+
+# @app.route('/scanned')
+# def scanned_page():  
+#     try:  
+#         return redirect(value)
+#     except:
+#         return redirect(url_for("video"))
+
+# @app.route('/video',methods=['GET'])
+# def video():
+#     try: 
+#         return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+#     except:
+#         return redirect(url_for("scanned_page"))
+
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
     if session.get('apple_id'):
@@ -71,20 +80,6 @@ def login_page():
     return render_template('login.html', form=form)
 
     
-
-@app.route('/logout')
-def logout_page():
-    if session:
-        session.clear()
-        flash("You have been logged out!", category='info')
-        return redirect(url_for("home_page"))
-    else:
-        session.clear()
-        flash("You have not logged in yet!", category='info')
-        return redirect(url_for("home_page"))
-
-
-
 
 @app.route('/logout')
 def logout_page():
