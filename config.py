94afefc7a8ada97cf4453ae77bf3aa0cf75e0c7e
Zoe Shen <rs6753@nyu.edu>
2022-12-06 15:36:59 -0500

    database

--- config.py
+++ config.py
@@ -8,12 +8,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae20295943'
                 
 #Connecting to the remote database
-conn = sql.connect(host='127.0.0.1',
-                   user='root',
-                   password='',
-                   db='ScannerDB',
-                   unix_socket='/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock',
+conn = sql.connect(host='sql.freedb.tech',
+                   user='freedb_nyudb',
+                   password='5?G3$vJ7c$8QSz4',
+                   db='freedb_scannerdb',
+                   unix_socket='',
                    charset='utf8mb4',
-                   cursorclass=pymysql.cursors.DictCursor)
+                   cursorclass=pymysql.cursors.DictCursor
+                )
 from register import register_bp
 app.register_blueprint(register_bp, url_prefix="/register")
