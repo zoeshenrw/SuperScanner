5f8b72a3db7e8aa1bbed01d4cdc034600e3a4007
andrewCoder2024 <67741322+andrewCoder2024@users.noreply.github.com>
2022-12-06 16:20:02 -0500

    fixed db

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae20295943'
                 
 #Connecting to the remote database
-conn = sql.connect(host='sql9.freesqldatabase.com',
-                   user='sql9581619',
-                   password='zYJJENMhQC',
-                   db='sql9581619',
+conn = sql.connect(host='sql.freedb.tech',
+                   user='freedb_nyudb',
+                   password='5?G3$vJ7c$8QSz4',
+                   db='freedb_scannerdb',
                    unix_socket='',
                    charset='utf8mb4',
                    cursorclass=pymysql.cursors.DictCursor
