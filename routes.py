a1eae9c549b058b4f2538e1433515b220e5033b9
andrewCoder2024 <67741322+andrewCoder2024@users.noreply.github.com>
2022-12-10 11:21:38 -0500

    removed unecessary code and fixed the push notifications aand added link functionality

--- routes.py
+++ routes.py
@@ -1,10 +1,10 @@
 from config import app, conn, distance, get_station_info, loc, URL_info 
-from flask import Flask, render_template, redirect, url_for, flash, request, Response, make_response, send_from_directory,session
+from flask import Flask, render_template, redirect, url_for, flash, request, Response, make_response, send_from_directory,session, Markup
 from pywebpush import webpush, WebPushException
 import json
 from forms import LoginForm
-import cv2
-import pyzbar.pyzbar as pyzbar
+#import cv2
+#import pyzbar.pyzbar as pyzbar
 import webbrowser
 import requests
 import math
@@ -13,42 +13,42 @@ import math
 global nearby_stations
 nearby_stations = get_station_info(URL_info, loc)
 
-#Notification
-VAPID_SUBJECT = "mailto:aml9186@nyu.edu"
-with open('private_key.json') as pk:
-    data = json.load(pk)
-VAPID_PRIVATE = data["public"]
-from register import register_bp
-app.register_blueprint(register_bp, url_prefix="/register")
+# #Notification
+# VAPID_SUBJECT = "mailto:aml9186@nyu.edu"
+# with open('private_key.json') as pk:
+#     data = json.load(pk)
+# VAPID_PRIVATE = data["public"]
+# from register import register_bp
+# app.register_blueprint(register_bp, url_prefix="/register")
 
-#Scanner
-camera=cv2.VideoCapture(0)
-global value 
-def read_qr_code(frame):
-    # Use cv2 to detect the QR code
-    try:
-        detect = cv2.QRCodeDetector()
-        value, points, straight_qrcode = detect.detectAndDecode(frame)
-        return value
-    except:
-        return None
+# #Scanner
+# camera=cv2.VideoCapture(0)
+# global value 
+# def read_qr_code(frame):
+#     # Use cv2 to detect the QR code
+#     try:
+#         detect = cv2.QRCodeDetector()
+#         value, points, straight_qrcode = detect.detectAndDecode(frame)
+#         return value
+#     except:
+#         return None
 
-#Read the camera frame
-def generate_frames():
-    while True:
-        success,frame=camera.read()
-        decoded=pyzbar.decode(frame)
-        if not success:
-            break
-        else:
-            value = read_qr_code(frame)
-            #Open web browser
-            if value:
-                webbrowser.open(value)
-                break
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
-        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+# #Read the camera frame
+# def generate_frames():
+#     while True:
+#         success,frame=camera.read()
+#         decoded=pyzbar.decode(frame)
+#         if not success:
+#             break
+#         else:
+#             value = read_qr_code(frame)
+#             #Open web browser
+#             if value:
+#                 webbrowser.open(value)
+#                 break
+#             ret,buffer=cv2.imencode('.jpg',frame)
+#             frame=buffer.tobytes()
+#         yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 #Home page
 @app.route('/')
@@ -58,36 +58,37 @@ def home_page():
     for station in nearby_stations:
         name  = station['station_name']
         capacity = station['station_capacity']
-        flash(f'Station nearby detected: location --> {name}\n There are {capacity} bikes remaining at the station', category='success')
+        lat, lon = station['station_lat'], station['station_lon']
+        flash(Markup(f'Station nearby detected: location --> {name}.There are {capacity} bikes remaining at the station. For Directions click <a href="https://www.google.com/maps/search/?api=1&query={lat},{lon}" class="alert-link">here</a>'), category='success')
     return render_template('home/home.html')
+#Markup('Successfully registered, please click <a href="/me" class="alert-link">here</a>')
+# # (B2) SERVICE WORKER
+# @app.route("/sw.js")
+# def sw():
+#   response = make_response(send_from_directory(app.static_folder, "sw.js"))
+#   return response
 
-# (B2) SERVICE WORKER
-@app.route("/sw.js")
-def sw():
-  response = make_response(send_from_directory(app.static_folder, "sw.js"))
-  return response
-
-#call this from a route when a nearby bike is found, and return the data to user as a push notification in json format
-@app.route("/push", methods=["POST"])
-def push():
-  sub = json.loads(request.form["sub"])
-  result = "OK"
-  try:
-    webpush(
-      subscription_info = sub,
-      data = json.dumps({
-        "title" : "Welcome!",
-        "body" : "Yes, it works!",
-        "icon" : "static/bikes.png",
-        "image" : "static/card.png"
-      }),
-      vapid_private_key = VAPID_PRIVATE,
-      vapid_claims = { "sub": VAPID_SUBJECT }
-    )
-  except WebPushException as ex:
-    print(ex)
-    result = "FAILED"
-  return result
+# #call this from a route when a nearby bike is found, and return the data to user as a push notification in json format
+# @app.route("/push", methods=["POST"])
+# def push():
+#   sub = json.loads(request.form["sub"])
+#   result = "OK"
+#   try:
+#     webpush(
+#       subscription_info = sub,
+#       data = json.dumps({
+#         "title" : "Welcome!",
+#         "body" : "Yes, it works!",
+#         "icon" : "static/bikes.png",
+#         "image" : "static/card.png"
+#       }),
+#       vapid_private_key = VAPID_PRIVATE,
+#       vapid_claims = { "sub": VAPID_SUBJECT }
+#     )
+#   except WebPushException as ex:
+#     print(ex)
+#     result = "FAILED"
+#   return result
 
 #About page
 @app.route('/about')
@@ -109,24 +110,24 @@ def market_page():
 def map_page():
     return render_template('map.html')
 
-#Scanner page
-@app.route('/scanner')
-def scanner_page():
-    return render_template('scanner.html')
+# #Scanner page
+# @app.route('/scanner')
+# def scanner_page():
+#     return render_template('scanner.html')
 
-@app.route('/scanned')
-def scanned_page():  
-    try:  
-        return redirect(value)
-    except:
-        return redirect(url_for("video"))
+# @app.route('/scanned')
+# def scanned_page():  
+#     try:  
+#         return redirect(value)
+#     except:
+#         return redirect(url_for("video"))
 
-@app.route('/video',methods=['GET'])
-def video():
-    try: 
-        return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
-    except:
-        return redirect(url_for("scanned_page"))
+# @app.route('/video',methods=['GET'])
+# def video():
+#     try: 
+#         return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+#     except:
+#         return redirect(url_for("scanned_page"))
 
 #Login page
 @app.route('/login', methods=['GET', 'POST'])
--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from config import app, conn
 from flask import session
 import register
 import routes
+from register import register_bp
 # Checks if the run.py file has executed directly and not imported
+app.register_blueprint(register_bp, url_prefix="/register")  
 if __name__ == '__main__':
     app.run(debug=True)
